d517159a93900cbc9f9c6cca87baf2eacd5a0315
Chad Glendenin <chad@glendenin.com>
2012-06-22 17:46:39 -0500

    basic node.js 'hello world' example

---
+++ web.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    app = express.createServer(express.logger()),
+    port = process.env.PORT || 5000;
+
+app.get('/', function (request, response) {
+    response.send('Hello World!');
+});
+
+app.listen(port, function () {
+    console.log('listening on port ' + port);
+});
